d11ff51d96ceea8d9b967e0f1c3f53490c04a297
Yoshiki <info@yoshiki-tanaka.work>
2020-09-08 16:35:35 +0900

    LPリニューアル

--- global/wp/wp-content/themes/twentytwenty/page-test.php
+++ global/wp/wp-content/themes/twentytwenty/page-test.php
@@ -51,8 +51,8 @@
         </div>
         <!-- /.mv -->
 
-        <div class="entry-btn">
-          <a href="#">いますぐお申込み</a>
+        <div class="entry-btn scroll-link">
+          <a href="#entry">いますぐお申込み</a>
         </div>
         <!-- /.entry-btn -->
 
@@ -107,10 +107,10 @@
       </section>
 
 
-      <section class="entry">
+      <section id="entry">
         <h2 class="entry__ttl">セミナーお申し込みは<br class="sp">こちらから</h2>
         <ul class="entry__period">
-          <li>開催日①：<br class="sp">2020年9月16日(水) 19時〜21時 <br class="sp">定員50名</li>
+          <li>開催日①：<br class="sp">2020年9月23日(水) 19時〜21時 <br class="sp">定員50名</li>
           <li>開催日②：<br class="sp">2020年9月26日(土) 11時〜13時 <br class="sp">定員50名</li>
         </ul>
 
@@ -121,33 +121,130 @@
             <table class="entry__form__table">
               <tr>
                 <th>
-                  <label for="Userpaymode" class="form_input_label">お支払い方法</label>
+                  <span class="required">
+                    <label for="Userpaymode" class="form_input_label">お支払い方法</label>
+                  </span>
                 </th>
                 <td>
                   <div class="entry__form__table__radio">
                     <input name="data[Proceed][paymode]" type="radio" id="Proceedpaymode_120_40" value="120_40" checked="checked" />
-                    <label for="Proceedpaymode_120_40" class="paymode_radio">クレジットカードでの一括払い<br class="sp">(代行業者：Stripe)</label>
+                    <label for="Proceedpaymode_120_40" class="paymode_radio" required="required">クレジットカードでの一括払い<br class="sp">(代行業者：Stripe)</label>
                   </div>
                 </td>
               </tr>
               <tr>
-                <th><label for="Username1" class="form_input_label ">姓</label></th>
+                <th>
+                  <span class="required">
+                    <label for="Username1" class="form_input_label ">姓</label>
+                  </span>
+                </th>
                 <td>
-                  <input name="data[User][name1]" id="Username1" value="" class="" type="text" />
+                  <input name="data[User][name1]" id="Username1" value="" class="required" type="text" required="required" />
                 </td>
               </tr>
               <tr>
-                <th><label for="Username2" class="form_input_label ">名</label></th>
-                <td><input name="data[User][name2]" id="Username2" value="" class="" type="text" /></td>
+                <th>
+                  <span class="required">
+                    <label for="Username2" class="form_input_label ">名</label>
+                  </span>
+                </th>
+                <td><input name="data[User][name2]" id="Username2" value="" class="required" type="text" required="required" /></td>
               </tr>
               <tr class="entry__form__table__mail">
                 <th>
-                  <span>
+                  <span class="required">
                     <label for="Usermail" class="form_input_label  required">メールアドレス</label>
                   </span>
                 </th>
                 <td>
                   <input name="data[User][mail]" id="Usermail" value="" class=" required" type="text" required="required" />
+                </td>
+              </tr>
+              <tr>
+                <th>
+                  <label for="Userpref" class="form_input_label  required">都道府県</label>
+                </th>
+                <td>
+                  <select name="data[User][pref]" id="Userpref" class="form_input_input  required">
+                    <option value="北海道">北海道</option>
+                    <option value="青森県">青森県</option>
+                    <option value="岩手県">岩手県</option>
+                    <option value="宮城県">宮城県</option>
+                    <option value="秋田県">秋田県</option>
+                    <option value="山形県">山形県</option>
+                    <option value="福島県">福島県</option>
+                    <option value="茨城県">茨城県</option>
+                    <option value="栃木県">栃木県</option>
+                    <option value="群馬県">群馬県</option>
+                    <option value="埼玉県">埼玉県</option>
+                    <option value="千葉県">千葉県</option>
+                    <option value="東京都" selected="selected">東京都</option>
+                    <option value="神奈川県">神奈川県</option>
+                    <option value="新潟県">新潟県</option>
+                    <option value="富山県">富山県</option>
+                    <option value="石川県">石川県</option>
+                    <option value="福井県">福井県</option>
+                    <option value="山梨県">山梨県</option>
+                    <option value="長野県">長野県</option>
+                    <option value="岐阜県">岐阜県</option>
+                    <option value="静岡県">静岡県</option>
+                    <option value="愛知県">愛知県</option>
+                    <option value="三重県">三重県</option>
+                    <option value="滋賀県">滋賀県</option>
+                    <option value="京都府">京都府</option>
+                    <option value="大阪府">大阪府</option>
+                    <option value="兵庫県">兵庫県</option>
+                    <option value="奈良県">奈良県</option>
+                    <option value="和歌山県">和歌山県</option>
+                    <option value="鳥取県">鳥取県</option>
+                    <option value="島根県">島根県</option>
+                    <option value="岡山県">岡山県</option>
+                    <option value="広島県">広島県</option>
+                    <option value="山口県">山口県</option>
+                    <option value="徳島県">徳島県</option>
+                    <option value="香川県">香川県</option>
+                    <option value="愛媛県">愛媛県</option>
+                    <option value="高知県">高知県</option>
+                    <option value="福岡県">福岡県</option>
+                    <option value="佐賀県">佐賀県</option>
+                    <option value="長崎県">長崎県</option>
+                    <option value="熊本県">熊本県</option>
+                    <option value="大分県">大分県</option>
+                    <option value="宮崎県">宮崎県</option>
+                    <option value="鹿児島県">鹿児島県</option>
+                    <option value="沖縄県">沖縄県</option>
+                  </select>
+                </td>
+              </tr>
+              <tr>
+                <th>
+                  <label for="Userzip" class="form_input_label ">住所</label>
+                </th>
+                <td>
+                  <input name="data[User][zip]" id="Userzip" value="" class="" type="text" />
+                </td>
+              </tr>
+              <tr>
+                <th>
+                  <span class="required">
+                    <label for="Usertel" class="form_input_label  required">電話番号</label>
+                  </span>
+                </th>
+                <td>
+                  <input name="data[User][tel]" id="Usertel" value="" class=" required" type="text" required="required" />
+                </td>
+              </tr>
+              <tr>
+                <th>
+                  <span class="required">
+                    <label for="Userfree1" class="form_input_label  required">参加予定日</label>
+                  </span>
+                </th>
+                <td>
+                  <select name="data[User][free1]" id="Userfree1" class="form_input_input  required" required="required">
+                    <option value="2020/9/23 19:00-21:00">2020/9/23 19:00-21:00</option>
+                    <option value="2020/9/26 11:00-13:00">2020/9/26 11:00-13:00</option>
+                  </select>
                 </td>
               </tr>
             </table>
@@ -170,45 +267,44 @@
             <!-- ▼リファラ -->
             <input type="hidden" name="data[User][referer_form_url]" value="" class="UserRefererFormUrl" />
             <input type="hidden" name="data[User][referer_url]" value="" class="UserRefererUrl" />
-
+            ​
             <script type="text/javascript">
-< !--
-    if (document.referrer.length != 0) {
-                if (document.getElementsByClassName("UserRefererUrl")) {
-                  var user_referer_url = document.getElementsByClassName("UserRefererUrl");
-                  for (var i = 0; i < user_referer_url.length; i++) {
-                    user_referer_url[i].value = document.referrer;
-                  }
+            < !--
+            if (document.referrer.length != 0) {
+              if (document.getElementsByClassName("UserRefererUrl")) {
+                var user_referer_url = document.getElementsByClassName("UserRefererUrl");
+                for (var i = 0; i < user_referer_url.length; i++) {
+                  user_referer_url[i].value = document.referrer;
                 }
-                else if (document.getElementById("UserRefererUrl")) {
-                  document.getElementById("UserRefererUrl").value = document.referrer;
-                }
+              } else if (document.getElementById("UserRefererUrl")) {
+                document.getElementById("UserRefererUrl").value = document.referrer;
               }
+            }
 
 
-              if (document.getElementsByClassName("UserRefererFormUrl")) {
-                var user_referer_form_url = document.getElementsByClassName("UserRefererFormUrl");
-                for (var i = 0; i < user_referer_form_url.length; i++) {
-                  user_referer_form_url[i].value = location.href;
-                }
+            if (document.getElementsByClassName("UserRefererFormUrl")) {
+              var user_referer_form_url = document.getElementsByClassName("UserRefererFormUrl");
+              for (var i = 0; i < user_referer_form_url.length; i++) {
+                user_referer_form_url[i].value = location.href;
               }
-              else if (document.getElementById("UserRefererFormUrl")) {
-                document.getElementById("UserRefererFormUrl").value = location.href;
-              }
-//-->
+            } else if (document.getElementById("UserRefererFormUrl")) {
+              document.getElementById("UserRefererFormUrl").value = location.href;
+            }
+            //-->
             </script>
             <!-- ▲リファラ -->
-
+            ​
           </form>
         </div>
+        <!-- /.entry__form -->
 
-        <script src="https://hero-t.com/js/validation.js?d=20200803181341"></script>
+        <script src="https://hero-t.com/js/validation.js?d=20200907174046"></script>
       </section>
 
       <section class="privacy">
         <h2 class="privacy__ttl">個人情報の管理・取扱いについて</h2>
         <p>当社では、お客様よりお預かりしました個人情報を、セミナーサービスやWebでの書籍、教材等の販売目的で利用いたします。<br>前述の利用目的達成の範囲内において業務委託する際には、個人情報を安全に管理できる委託先を選定した上で当該委託先を適切に監督いたします。なお、必要事項の中でご提出いただけない個人情報がある場合、サービスの一部をお受けいただけない場合がございますのでご了承下さい。個人情報の利用目的の通知、開示、内容の訂正、追加又は削除、利用の停止、消去及び第三者への提供の停止については、下記までご連絡ください。</p>
-        
+
         <p>ザ・ヒーロートレーニング事務局 お客様相談窓口<br>TEL. 06-6123-7650</p>
       </section>
     </article>
@@ -248,5 +344,23 @@
     </div>
     <!-- /.footer__wrap -->
   </footer>
+
+  <script>
+  $(function() {
+    // スクロールアニメーション
+    $('.scroll-link a').on('click', function(event) {
+      event.preventDefault();
+
+      var $this = $(this);
+      var linkTo = $this.attr('href');
+      var $target = $(linkTo);
+      var offset = 0;
+      var pos = $target.offset().top - offset;
+      $('html,body').animate({
+        scrollTop: pos
+      }, 400);
+    });
+  });
+  </script>
 </body>
 </html>
