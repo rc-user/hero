b5aa9b778e6bae5a08977899c25278b6645508a4
澤田諒二 <>
2020-12-25 17:39:51 +0900

    css無理やり追加

--- wp/wp-content/themes/heros/single.php
+++ wp/wp-content/themes/heros/single.php
@@ -1,20 +1,52 @@
 <?php get_header(); ?>
 
+<style>
+  .media_wrap{
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 0 15px;
+  }
+  
+  .media_wrap *{
+    line-height: 3;
+  }
+  
+  .media_wrap h1{
+    font-size: 24px;
+    font-weight: bold;
+    margin-bottom: 30px;
+    line-height: 1.4;
+  }
+  .wp-block-embed-youtube{
+    text-align: center;
+  }
+  
+  .media_wrap img{
+    width: 100%;
+    height: auto;
+  }
+
+</style>
+
+
+
 <main id="main">
   <article>
-    <section class="page__mv">
-      <h1 class="page__mv__ttl">
-        <?php the_title(); ?>
-      </h1>
-    </section>
+    <div class="media_wrap">
+      <section class="page__mv">
+        <h1 class="page__mv__ttl">
+          <?php the_title(); ?>
+        </h1>
+      </section>
 
 
-    <section class="page__contents">
-      <?php if(have_posts()):while(have_posts()):the_post(); ?>
-      <?php the_content(); ?>
-      <?php endwhile;else: ?>
-      <?php endif; ?>
-    </section>
+      <section class="page__contents">
+        <?php if(have_posts()):while(have_posts()):the_post(); ?>
+        <?php the_content(); ?>
+        <?php endwhile;else: ?>
+        <?php endif; ?>
+      </section>
+    </div>  
   </article>
 </main>
 
